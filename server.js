198793199575c273914e7189c41aeaf10f21b7c8
Hideki515 <brunohideki515@gmail.com>
2025-02-24 11:54:25 -0300

    ♻️ refactor: Renomeia e move a rota de atualização de receitas por ano e mês para um novo arquivo

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import swaggerUi from '@fastify/swagger-ui';
 import { getRevenuesByAccountRoute } from './src/routes/get-revenues-by-account-route.js';
 import { getRevenuesByYearAndMonthRoute } from './src/routes/get-revenues-by-yearAndMonth-route.js';
 import { postRevenuesRoute } from './src/routes/post-revenues.js';
-import { patchRevenuesRoute } from './src/routes/patch-revenues.js';
+import { patchRevenuesRoute } from './src/routes/patch-revenues-route.js';
 import { deleteRevenueByIdRoute } from './src/routes/delete-revenue-by-id-routes.js';
 
 const app = fastify();
