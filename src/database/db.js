fb0cacd9fc986e9ef8edf384a8efa2eb5d14ca0a
Hideki515 <brunohideki515@gmail.com>
2025-02-27 08:37:31 -0300

    :heavy_plus_sign: build: Corrigindo variáveis de ambiente para DB Online

--- src/database/db.js
+++ src/database/db.js
@@ -4,9 +4,11 @@ import mysql from 'mysql2';
 // Conectar ao banco de dados
 const db = mysql.createConnection({
   host: env.DB_HOST,
+  port: env.DB_PORT,
   user: env.DB_USER,
   password: env.DB_PASSWORD,
-  database: env.DB_DATABASE
+  database: env.DB_DATABASE,
+  uri: env.DB_URI,
 });
 
 export default db;
